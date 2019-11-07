a4f40864611be2060407818c5903cf85e5a73c63
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-07 04:37:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {"the beatles": "Blackbird", "radiohead": "Just", "kanye west": "Good Morning"};
 
-function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {[artist]: song});
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
 }
